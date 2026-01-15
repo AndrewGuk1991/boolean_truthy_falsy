b189e96a3283acaae0883d8106b39696efbe2326
andrew_guk <gukandrey91@gmail.com>
2026-01-15 19:40:15 +0300

    tasks with one operator ||

---
+++ operator_or.js
@@ -0,0 +1,6 @@
+console.log(false || "Default") // "Default"
+console.log("" || "Fallback") // "Fallback"
+console.log(null || "Backup") // "Backup"
+console.log("First" || "Second") // "First"
+console.log(0 || 42) // 42
+console.log(undefined || "Value") // "Value"
