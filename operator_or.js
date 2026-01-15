9b5860bfad53d8ccd09dfd24983ad1d5fd948357
andrew_guk <gukandrey91@gmail.com>
2026-01-15 19:42:38 +0300

    tasks with two and more operators ||

--- operator_or.js
+++ operator_or.js
@@ -4,3 +4,9 @@ console.log(null || "Backup") // "Backup"
 console.log("First" || "Second") // "First"
 console.log(0 || 42) // 42
 console.log(undefined || "Value") // "Value"
+
+
+console.log("" || 0 || "Found") // "Found"
+console.log(false || null || "OK") // "OK"
+console.log(undefined || "" || 0 || "Final") // "Final"
+console.log("Start" || false || "End") // "Start"
